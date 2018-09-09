429c5a1bb709cdffd333545ae9b9663821a7d98d
Nathan Pabst <nathan.pabst@gmail.com>
2018-09-09 16:40:00 -0500

    foxies printing as expected

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,23 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myPeeps = new Dictionary<string, Dictionary<string, string>>();
+
+            myPeeps.Add("Mom", new Dictionary<string, string> {
+                { "name", "MommaFox" },
+                { "age", "42" }
+            });
+            myPeeps.Add("Dad", new Dictionary<string, string> {
+                { "name", "FoxyPop" },
+                { "age", "42" }
+            });
+
+            foreach (var i in myPeeps)
+            {
+                Console.WriteLine($"{i.Value["name"]} is my {i.Key} and is {i.Value["age"]} years young.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
